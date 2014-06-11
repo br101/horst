8b3ee845c1d253f69f83711a90e788df54f13e32
Bruno Randolf <br1@einfach.org>
2014-06-11 13:36:27 +0100

    fix help screen packet types

--- display-help.c
+++ display-help.c
@@ -39,27 +39,27 @@ update_help_win(WINDOW *win)
 	print_centered(win, 2, COLS, "HORST - Horsts OLSR Radio Scanning Tool");
 	print_centered(win, 3, COLS, "Version " VERSION " (build date " __DATE__ " " __TIME__ ")");
 
-	mvwprintw(win, 5, 2, "(C) 2005-2011 Bruno Randolf, Licensed under the GPLv2");
+	mvwprintw(win, 5, 2, "(C) 2005-2014 Bruno Randolf, Licensed under the GPLv2");
 
 	mvwprintw(win, 7, 2, "Known IEEE802.11 Packet Types:");
 	l = 9;
 	/* this is weird but it works */
 	mvwprintw(win, l++, 2, "MANAGEMENT FRAMES");
-	for (i = 0x00; i <= 0xD0; i = i + 0x10) {
+	for (i = 0x00; i <= 0xE0; i = i + 0x10) {
 		c = get_packet_type_char(i);
 		if (c != '?')
 			mvwprintw(win, l++, 4, "%c %s", c, get_packet_type_name(i));
 	}
 	l = 9;
 	mvwprintw(win, l++, 25, "CONTROL FRAMES");
-	for (i = 0xa4; i <= 0xF4; i = i + 0x10) {
+	for (i = 0x74; i <= 0xF4; i = i + 0x10) {
 		c = get_packet_type_char(i);
 		if (c != '?')
 			mvwprintw(win, l++, 27, "%c %s", c, get_packet_type_name(i));
 	}
 	l = 9;
 	mvwprintw(win, l++, 50, "DATA FRAMES");
-	for (i = 0x08; i <+ 0xF8; i = i + 0x10) {
+	for (i = 0x08; i <= 0xF8; i = i + 0x10) {
 		c = get_packet_type_char(i);
 		if (c != '?')
 			mvwprintw(win, l++, 52, "%c %s", c, get_packet_type_name(i));
