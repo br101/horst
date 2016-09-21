br101/horst

commit c71563a985ebae941219d8ff7920ba1bc027f396

Use clock_gettime() for monotonically increasing time
This is important for avoiding problems when system time gets changed

main.c
@@ -53,7 +53,7 @@ struct node_names_info node_names;
 
 struct config conf;
 
-struct timeval the_time;
+struct timespec the_time;
 
 int mon; /* monitoring socket */
 
@@ -214,7 +214,7 @@ static void write_to_file(struct packet_info* p)
 
 	//timestamp, e.g. 2015-05-16 15:05:44.338806 +0300
 	i = strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ltm);
-	i += snprintf(buf + i, sizeof(buf) - i, ".%06ld", (long)(the_time.tv_usec));
+	i += snprintf(buf + i, sizeof(buf) - i, ".%06ld", (long)(the_time.tv_nsec / 1000));
 	i += strftime(buf + i, sizeof(buf) - i, " %z", ltm);
 	fprintf(DF, "%s, ", buf);
 
@@ -634,8 +634,8 @@ int main(int argc, char** argv)
 
 	atexit(exit_handler);
 
-	gettimeofday(&stats.stats_time, NULL);
-	gettimeofday(&the_time, NULL);
+	clock_gettime(CLOCK_MONOTONIC, &stats.stats_time);
+	clock_gettime(CLOCK_MONOTONIC, &the_time);
 
 	conf.channel_idx = -1;
 
@@ -720,7 +720,7 @@ int main(int argc, char** argv)
 		if (is_sigint_caught)
 			exit(1);
 
-		gettimeofday(&the_time, NULL);
+		clock_gettime(CLOCK_MONOTONIC, &the_time);
 		timeout_nodes();
 
 		if (conf.serveraddr[0] == '\0') { /* server */
@@ -757,7 +757,7 @@ void main_reset()
 	memset(&stats, 0, sizeof(stats));
 	memset(&spectrum, 0, sizeof(spectrum));
 	init_spectrum();
-	gettimeofday(&stats.stats_time, NULL);
+	clock_gettime(CLOCK_MONOTONIC, &stats.stats_time);
 }
 
 void dumpfile_open(const char* name)

channel.c
@@ -27,7 +27,7 @@
 #include "wlan_util.h"
 
 
-static struct timeval last_channelchange;
+static struct timespec last_channelchange;
 static struct channel_list channels;
 
 long channel_get_remaining_dwell_time(void)
@@ -37,9 +37,9 @@ long channel_get_remaining_dwell_time(void)
 
 	return conf.channel_time
 		- the_time.tv_sec * 1000000
-		- the_time.tv_usec
+		- the_time.tv_nsec / 1000
 		+ last_channelchange.tv_sec * 1000000
-		+ last_channelchange.tv_usec;
+		+ last_channelchange.tv_nsec / 1000;
 }
 
 
@@ -177,7 +177,7 @@ bool channel_change(int idx, enum chan_width width, bool ht40plus)
 		channels.chan[idx].chan, channels.chan[idx].freq,
 		channel_width_string(width, ht40plus),
 		 center1, (the_time.tv_sec - last_channelchange.tv_sec) * 1000
-		 + (the_time.tv_usec - last_channelchange.tv_usec) / 1000);
+		 + (the_time.tv_nsec - last_channelchange.tv_nsec) / 1000000);
 
 	conf.channel_idx = idx;
 	conf.channel_width = width;

display.c
@@ -37,7 +37,7 @@ static WINDOW *show_win = NULL;
 static int conf_win_current;
 static int show_win_current;
 
-static struct timeval last_time;
+static struct timespec last_time;
 
 static int display_resize_needed = 0;
 
@@ -71,14 +71,14 @@ void get_per_second(unsigned long bytes, unsigned long duration,
 	       unsigned long packets, unsigned long retries,
 	       int *bps, int *dps, int *pps, int *rps)
 {
-	static struct timeval last;
+	static struct timespec last;
 	static unsigned long last_bytes, last_dur, last_pkts, last_retr;
 	static int last_bps, last_dps, last_pps, last_rps;
 	float timediff;
 
 	/* reacalculate only every second or more */
-	timediff = (the_time.tv_sec + the_time.tv_usec/1000000.0) -
-		   (last.tv_sec + last.tv_usec/1000000.0);
+	timediff = (the_time.tv_sec + the_time.tv_nsec/1000000000.0) -
+		   (last.tv_sec + last.tv_nsec/1000000000.0);
 	if (timediff >= 1.0) {
 		last_dps = (1.0*(duration - last_dur)) / timediff;
 		last_bps = (1.0*(bytes - last_bytes)) / timediff;
@@ -317,7 +317,7 @@ void update_display(struct packet_info* pkt)
 	 */
 	if (pkt != NULL &&
 	    the_time.tv_sec == last_time.tv_sec &&
-	    (the_time.tv_usec - last_time.tv_usec) < conf.display_interval ) {
+	    (the_time.tv_nsec - last_time.tv_nsec) / 1000 < conf.display_interval ) {
 		/* just add the line to dump win so we don't loose it */
 		update_dump_win(pkt);
 		return;

main.h
@@ -21,7 +21,7 @@
 #define _MAIN_H_
 
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 
 #undef LIST_HEAD 
 #include "ccan/list/list.h"
@@ -252,7 +252,7 @@ struct statistics {
 
 	unsigned long		filtered_packets;
 
-	struct timeval		stats_time;
+	struct timespec		stats_time;
 };
 
 extern struct statistics stats;
@@ -345,7 +345,7 @@ struct config {
 
 extern struct config conf;
 
-extern struct timeval the_time;
+extern struct timespec the_time;
 
 void free_lists(void);
 void init_spectrum(void);

node.c
@@ -26,7 +26,7 @@
 #include "wlan80211.h"
 #include "essid.h"
 
-static struct timeval last_nodetimeout;
+static struct timespec last_nodetimeout;
 
 static void copy_nodeinfo(struct node_info* n, struct packet_info* p)
 {

commit 7e51f1670b8e21eddcca0b3d4e76bfa089f3c23e

Fix #47, Implement `clock_gettime` wrapper for Mac OS X / Mach
This fixes a compile problem on Mac OS X that was introduced in c71563a985ebae941219d8ff7920ba1bc027f396 when the `gettimeofday` function was replaced with `clock_gettime`, the latter of which is not available on OS X.

In particular, this commit
* Adds a small wrapper library, `mach_clock_gettime`, that transparently handles calls to `clock_gettime` by appropriately redirecting them to OS X's `clock_get_time` function,
* Adds a conditional include to `main.c` so that systems that define `__MACH__` use the wrapper lib,
* Adds the wrapper lib to the list of object files in `Makefile` that will be built if the user sets the `OSX` build option on the command line.

Makefile
@@ -59,6 +59,7 @@ ifeq ($(OSX),1)
     WEXT=0
     LIBNL=0
     LIBS+=-framework CoreWLAN -framework CoreData -framework Foundation
+    OBJS+=mach_clock_gettime.o
 endif
 
 ifeq ($(DEBUG),1)

mach_clock_gettime.c
@@ -1,0 +1,68 @@
+/*
+ * mach_clock_gettime --- an emulation of POSIX's `clock_gettime` for
+ * Mach / Mac OS X.
+ *
+ * This library emulates a function from the POSIX Realtime Extensions
+ * for getting monotonically increasing clock readings (`clock_gettime`
+ * with a `clock_id` of `CLOCK_MONOTONIC`) using Mac OS X's `clock_get_time`
+ * call on the monotonically increasing `SYSTEM_CLOCK` clock service.
+ *
+ * It is designed as a drop-in replacement for that specific purpose:
+ * Do a conditional include of this library on systems that define
+ * __MACH__, then use `clock_gettime(CLOCK_MONOTONIC, &...)` as if on
+ * a system that implements the POSIX Realtime Extensions.
+ *
+ * Other clock sources, alarms, etc. are currently not supported.
+ *
+ * This library is based on material by
+ * - The Open Group, see
+ *     http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_gettime.html
+ * - the Linux kernel headers, see time.h, posix_types.h and types.h
+ * - The XNU man pages, see host_get_clock_service, clock_get_time and
+     tvalspec under https://opensource.apple.com/source/xnu/xnu-3248.60.10/osfmk/man/
+ * - https://stackoverflow.com/questions/11680461/monotonic-clock-on-osx
+ *
+ * Other similar (and probably more complete) approaches exist and inspired
+ * this implementation, see for instance
+ * - https://gist.github.com/jbenet/1087739
+ * - https://gist.github.com/alfwatt/3588c5aa1f7a1ef7a3bb
+ * - https://github.com/ChisholmKyle/PosixMachTiming
+ *
+ * Licensing note: Given its vanishing amount of originality, this library
+ * is placed under a CC0 license, i.e. I release it into the public domain.
+ * See also https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ *
+ * Please send bug reports to albert.rafetseder@univie.ac.at
+ */
+
+#include <sys/types.h>
+#include <mach/clock.h>
+#include <mach/mach.h>
+#include <err.h>
+
+#include "mach_clock_gettime.h"
+
+int clock_gettime(clockid_t clock_id, struct timespec *tp)
+{
+	/* The XNU docs prescribe `clock_t` and `tvalspec_t` for the
+	 * first two variables; we however use what the header file
+	 * in /usr/include/mach/clock.h tells us to (or else the
+	 * preprocessor complains). */
+	clock_serv_t clock_name;
+	mach_timespec_t current_clock_value;
+        kern_return_t retval;
+
+	/* Bail out on clock_id's that we don't emulate */
+	if (clock_id != CLOCK_MONOTONIC)
+		err(1, "Unsupported clock_id for mach_clock_gettime emulation");
+
+	/* XXX This could use some more error checking... */
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &clock_name);
+	retval = clock_get_time(clock_name, &current_clock_value);
+	mach_port_deallocate(mach_task_self(), clock_name);
+	tp->tv_sec = current_clock_value.tv_sec;
+	tp->tv_nsec = current_clock_value.tv_nsec;
+
+	return retval;
+}
+

mach_clock_gettime.h
@@ -1,0 +1,20 @@
+/*
+ * mach_clock_gettime --- an emulation of POSIX's `clock_gettime` for
+ * Mach / Mac OS X.
+ *
+ * See mach_clock_gettime.c for details.
+ */
+
+#ifndef _MACH_CLOCK_GETTIME_H_
+#define _MACH_CLOCK_GETTIME_H_
+
+#include <time.h>
+
+#define CLOCK_MONOTONIC 1	/* Per Linux's time.h */
+typedef int clockid_t;		/* Per Linux's types.h, posix_types.h */
+
+/* Per the POSIX Realtime Extensions */
+int clock_gettime(clockid_t clock_id, struct timespec *tp);
+
+#endif
+

main.c
@@ -44,6 +44,10 @@
 #include "conf_options.h"
 #include "ifctrl.h"
 
+#ifdef __MACH__
+#include "mach_clock_gettime.h"
+#endif
+
 struct list_head nodes;
 struct essid_meta_info essids;
 struct history hist;